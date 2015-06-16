bde34a7786295d2a6a38e10c09afeffcbb2fb06f
Saptanto Sindu K U <saptanto.sindu@gmail.com>
2015-06-16 15:28:44 +0700

    Tambah Menu

--- src/model/MenuModel.java
+++ src/model/MenuModel.java
@@ -22,9 +22,8 @@ import java.util.Arrays;
 public class MenuModel {
     Connection conn = new DBConnection().connect();
     
-    private String kodeMenu, namaMenu;
-    private int harga, kategori;
-    private ArrayList baris = new ArrayList();
+    private String kodeMenu, namaMenu, keterangan;
+    private int harga;
     private ArrayList menu = new ArrayList();
     
     public ArrayList getMenuContent(){
@@ -33,17 +32,62 @@ public class MenuModel {
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet hasil = ps.executeQuery();
             while(hasil.next()){
-                baris.add(hasil.getString("kode_menu"));
-                baris.add(hasil.getString("nama"));
-                baris.add(hasil.getString("kategori"));
-                baris.add(hasil.getInt("kategori"));
-                baris.add(hasil.getInt("harga"));
-                menu.add(baris);
-                baris.clear();
+                Object[] o;
+                o = new Object[4];
+                o[0] = (hasil.getString("kodeMenu"));
+                o[1] = (hasil.getString("namaMenu"));
+                o[2] =( hasil.getString("keterangan"));
+                o[3] = (hasil.getInt("harga"));
+                menu.add(o);
             }
         }catch(Exception ex){
             System.out.println(ex);
         }
         return menu;
+    }
+    
+    public String insertMenu(String kodeMenu, String namaMenu, String keterangan, int harga){
+        String sql = "INSERT INTO tbl_menu (kodeMenu, namaMenu, keterangan, harga) VALUES (?, ?, ?, ?)";
+        try{
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, kodeMenu);
+            ps.setString(2, namaMenu);
+            ps.setString(3, keterangan);
+            ps.setInt(4, harga);
+            ps.executeUpdate();
+        }catch(Exception e){
+            System.out.println(e);
+            return "gagal";
+        }
+        return "sukses";
+    }
+    
+    public String updateMenu(String kodeMenu, String namaMenu, String keterangan, int harga){
+        String sql = "UPDATE tbl_menu SET kodeMenu = ?, namaMenu = ?, keterangan = ?, harga = ?";
+        try{
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, kodeMenu);
+            ps.setString(2, namaMenu);
+            ps.setString(3, keterangan);
+            ps.setInt(4, harga);
+            ps.executeUpdate();
+        }catch(Exception e){
+            System.out.println(e);
+            return "gagal";
+        }
+        return "sukses";
+    }
+    
+    public String deleteMenu(String kodeMenu){
+        String sql = "DELETE FROM tbl_menu WHERE kodeMenu = ?";
+        try{
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, kodeMenu);
+            ps.executeUpdate();
+        }catch(Exception e){
+            System.out.println(e);
+            return "gagal";
+        }
+        return "sukses";
     }
 }
